1d8e42cdfdf83c5614d9b93c64465b027642f69e
Valentin Mary <valentin.mary@etu.univ-amu.fr>
2026-06-10 14:06:38 +0200

    First commit

---
+++ src/main/java/fr/vamary/danativel/blog/spring/rest/api/Application.java
@@ -0,0 +1,13 @@
+package fr.vamary.danativel.blog.spring.rest.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Application.class, args);
+	}
+
+}
---
+++ src/main/java/fr/vamary/danativel/blog/spring/rest/api/entity/Article.java
@@ -0,0 +1,37 @@
+package fr.vamary.danativel.blog.spring.rest.api.entity;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.NonNull;
+import lombok.Setter;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
+
+import java.time.Instant;
+
+@Entity
+@Getter @Setter
+@Table(name = "articles")
+public class Article {
+
+    @Id
+    @GeneratedValue(strategy= GenerationType.AUTO)
+    private Long id;
+
+    private String title;
+
+    private String description;
+
+    private String content;
+
+    @ManyToOne
+    @JoinColumn(name = "author_id")
+    private User author;
+
+    @CreationTimestamp
+    private Instant created;
+
+    @UpdateTimestamp
+    private Instant updated;
+
+}
